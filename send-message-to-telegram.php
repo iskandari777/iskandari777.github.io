5021fa24669cb534ef74e53143ef3e02c6acc6f7
billi-uzz <33725412+iskandari777@users.noreply.github.com>
2018-05-31 13:08:08 +0500

    Add files via upload

--- send-message-to-telegram.php
+++ send-message-to-telegram.php
@@ -30,7 +30,7 @@ $arr = array(
 foreach($arr as $key => $value) {
   $txt .= "<b>".$key."</b> ".$value."%0A";
 };
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+$sendToTelegram = fopen("https://api.telegram.org/bot{386022962:AAEB3ks2fSrExTDhde6ZPtKLo-ZFx3B6Gnw}/sendMessage?chat_id={-305991601}&parse_mode=html&text={$txt}","r");
 if ($sendToTelegram) {
   
   echo '<p class="success">Спасибо за отправку вашего сообщения!</p>';
