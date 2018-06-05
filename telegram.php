d683c8cbfb54c0c532e78e0450be113cdb552969
billi-uzz <33725412+iskandari777@users.noreply.github.com>
2018-06-05 13:29:58 +0500

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+
+$name=$_POST['user_name'];
+$phone=$_POST['user_phone'];
+$email=$_POST['user_email'] ;
+$token="386022962:AAEB3ks2fSrExTDhde6ZPtKLo-ZFx3B6Gnw";
+$chat_id = "-305991601";
+$arr=array(
+'Ismi:'=>$name,
+'Telefon:'=>$phone,
+'Email:'=>$email
+);
+
+foreach ($arr as $key => $value) {
+	$txt .="<b>" .$key. "</b>" .$value. "$0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}$parse_mode=html&text={$txt}", "r");
+if ($sendToTelegram) {
+	header('Location:thank_you.html');
+}
+else{
+	echo "Error";
+}
+
+?>
